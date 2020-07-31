857f8f6e09cb71c8d21d254764f4bee4de353ed7
G.Koganovskiy <grisha.koganovskiy@gmail.com>
2020-07-31 22:43:22 +0300

    add reorgnizing function to parser

--- Parser.py
+++ Parser.py
@@ -103,45 +103,135 @@ def pearson_correlation(line_1, line_2):
     return correlation_coefficient
 
 
+'''This function finds all user ratings of given anime and returns them'''
+
+
+def find_ratings(anime_id, rating_list, user_ids):
+    ratings = []
+    for review in rating_list:
+        if int(review['anime_id']) == anime_id:
+            user_id = int(review['user_id'])
+            rating = int(review['rating'])
+            if rating == -1:
+                rating = None
+
+            if rating is not None:
+                record = [user_id, rating]
+                ratings.append(record)
+    return ratings
+
+
+'''This function filters ratings for two titles. In the end there are only ratings from users that watched both'''
+
+
+def get_matching_ratings(ratings_1, ratings_2):
+    result_1 = []
+    result_2 = []
+    index_1 = 0
+    index_2 = 0
+    while index_1 < len(ratings_1) and index_2 < len(ratings_2):
+        if ratings_1[index_1][0] < ratings_2[index_2][0]:
+            index_1 += 1
+            continue
+        if ratings_2[index_2][0] < ratings_1[index_1][0]:
+            index_2 += 1
+            continue
+        if ratings_1[index_1][0] == ratings_2[index_2][0]:
+            result_1.append(ratings_1[index_1][1])
+            result_2.append(ratings_2[index_2][1])
+            index_1 += 1
+            index_2 += 1
+            continue
+    return result_1, result_2
+
+
+'''This function returns ratings for given anime title'''
+
+
+def get_ratings(anime_id):
+    return 42
+
+
+'''This function computes pearson correlation score for two given anime titles'''
+
+
 def compute_anime_correlation(id_1, id_2):
     global rating_list, anime_list
-    print(id_1, id_2)
     for anime in anime_list:
-        if int(anime['anime_id']) == id_1 or int(anime['anime_id']) == id_2:
-            print(anime['name'])
+        if int(anime['anime_id']) == id_1:
+            print('First Anime: ' + anime['name'] + ' (' + str(id_1) + ')')
+        if int(anime['anime_id']) == id_2:
+            print('Second Anime: ' + anime['name'] + ' (' + str(id_2) + ')')
+    print('Getting ratings')
+    first_ratings = get_ratings(id_1)
+    second_ratings = get_ratings(id_2)
+    first_ratings, second_ratings = get_matching_ratings(first_ratings, second_ratings)
+    correlation_coefficient = pearson_correlation(first_ratings, second_ratings)
+    print(correlation_coefficient)
+    return correlation_coefficient
 
 
-if __name__ == '__main__':
+'''This function reorganizes 'rating.csv' file to more convenient format and writes it to 'ReorganizedDB.txt' file'''
+
+
+def compress_ratings_file():
     # importing libs for file management
     import csv
     import codecs
 
     # reading files
+    print('Loading data')
     rating_file = codecs.open('rating.csv', 'r', 'utf_8_sig')
     anime_file = codecs.open('anime.csv', 'r', 'utf_8_sig')
 
     # making ordered dictionaries from files
+    print('Preparing data')
     rating_ordered_dict = csv.DictReader(rating_file)
     anime_ordered_dict = csv.DictReader(anime_file)
 
-    # converting ordered dictionaries to usual dictionaries
+    # converting ordered dictionaries to object lists
     rating_list = convert_to_list(rating_ordered_dict)
     anime_list = convert_to_list(anime_ordered_dict)
 
+    print('Getting object ids')
     # getting list of all anime ids
     anime_ids = []
     for anime in anime_list:
-        anime_ids.append(anime['anime_id'])
+        anime_ids.append(int(anime['anime_id']))
 
+    # getting list of all user ids
+    user_ids = []
+    for rating in rating_list:
+        if len(user_ids) == 0 or int(rating['user_id']) != user_ids[-1]:
+            user_ids.append(int(rating['user_id']))
 
-    # compute_anime_correlation(32281, 28977)
+    print('Reorganizing data')
+    import time
+    anime_ratings = []
+    for i in range(5):
+        begin_time = time.process_time()
+        anime_id = anime_ids[i]
+        ratings = find_ratings(anime_id, rating_list, user_ids)
+        line = ''
+        line += ('[' + str(anime_id) + ']')
+        for review in ratings:
+            user_id = review[0]
+            rating = review[1]
+            line += (',(' + str(user_id) + '-' + str(rating) + ')')
+        line += '\n'
+        anime_ratings.append(line)
+        end_time = time.process_time()
+        done_percent = (i + 1) / len(anime_ids) * 100
+        print('Done ' + str(done_percent) + '% in ' + str(end_time - begin_time) + ' seconds')
+
+    reorganized_db = open('ReorganizedDB.txt', 'w')
+    reorganized_db.writelines(anime_ratings)
 
     # closing files
     rating_file.close()
     anime_file.close()
+    reorganized_db.close()
 
-'''
-Main logic:
-1). There is a function that i give to anime id's and it spits out correlation score for them
-2). I have a csv table where cell with coordinats x and y contains correlation score for anime x and y
-'''
+
+if __name__ == '__main__':
+    compress_ratings_file()
