e88b3c46e1a0f5ca958ace88cf71e8f07413b172
G.Koganovskiy <grisha.koganovskiy@gmail.com>
2020-08-27 22:00:54 +0300

    Recommender optimized with threading

--- Anime-Recommender(v1.1)/Modules/Recommender/Recommender.py
+++ Anime-Recommender(v1.1)/Modules/Recommender/Recommender.py
@@ -1,4 +1,4 @@
-import sqlite3
+import concurrent.futures
 import Guesser
 
 
@@ -17,6 +17,20 @@ def get_input_for(anime_id, user=None, anime_ids=None):
     return [crowd_rating, members, corr_score, genre_score]
 
 
+def get_estimated_score(anime_id, factors, user=None, anime_ids=None):
+    if not user:
+        user = Guesser.get_user_object()
+    if not anime_ids:
+        anime_ids = Guesser.get_anime_ids()
+
+    args = get_input_for(anime_id, user=user, anime_ids=anime_ids)
+    result = 0
+    for arg_ind in range(len(args)):
+        result += args[arg_ind] * factors[arg_ind]
+    record = [result, anime_id]
+    return record
+
+
 def get_recs(factors, user=None, anime_ids=None, progress_log=False):
     if not user:
         user = Guesser.get_user_object()
@@ -24,24 +38,25 @@ def get_recs(factors, user=None, anime_ids=None, progress_log=False):
         anime_ids = Guesser.get_anime_ids()
 
     recommendations = []
-    for ind, anime_id in enumerate(anime_ids, start=1):
-        args = get_input_for(anime_id, user=user, anime_ids=anime_ids)
-        result = 0
-        for i in range(len(args)):
-            result += args[i] * factors[i]
-        record = [result, anime_id]
-        if progress_log:
-            print(f'{round(ind / len(anime_ids) * 100, 2)}% Done, record: {record}')
-        recommendations.append(record)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        threads = []
+        for anime_id in anime_ids:
+            threads.append(executor.submit(get_estimated_score, anime_id, factors, user=user, anime_ids=anime_ids))
+
+        for thread in concurrent.futures.as_completed(threads):
+            record = thread.result()
+            recommendations.append(record)
+            if progress_log:
+                print(f'{len(recommendations) / len(anime_ids) * 100}% Done')
 
     recommendations.sort(reverse=True)
     return recommendations
 
 
 if __name__ == '__main__':
-    user = Guesser.get_user_object()
-    anime_ids = Guesser.get_anime_ids()
-    user_factors = Guesser.get_user_factors(user=user, progress_log=True)
-    recs = get_recs(user_factors, user=user, anime_ids=anime_ids, progress_log=True)
+    user_obj = Guesser.get_user_object()
+    anime_ids_list = Guesser.get_anime_ids()
+    user_factors = Guesser.get_user_factors(user=user_obj, progress_log=True)
+    recs = get_recs(user_factors, user=user_obj, anime_ids=anime_ids_list, progress_log=True)
     for i in range(10):
         print(recs[i])
