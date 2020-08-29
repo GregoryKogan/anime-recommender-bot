6f2fc0caead0b017fe03105a171b5f3369fbe42d
G.Koganovskiy <grisha.koganovskiy@gmail.com>
2020-08-29 16:44:58 +0300

    added gitignore

--- Telegram-Recommender/RecommenderBot.py
+++ Telegram-Recommender/RecommenderBot.py
@@ -1,15 +1,24 @@
 import telebot
+from telebot import types
 import config
 
 bot = telebot.TeleBot(config.API_TOKEN)
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.send_message(message.chat.id, f'Sup, {message.from_user.first_name}')
-    print(f"""Message: {message.text}
-Chat: {message.chat.id}
-From: {message.from_user.username}""")
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    add_anime_button = types.KeyboardButton('Add anime')
+    ban_anime_button = types.KeyboardButton('Ban Anime')
+    account_button = types.KeyboardButton('Account')
+    ban_list_button = types.KeyboardButton('Ban list')
+    get_recs_button = types.KeyboardButton('Get recommendations')
+    markup.add(add_anime_button,
+               ban_anime_button,
+               account_button,
+               ban_list_button,
+               get_recs_button)
+    bot.send_message(message.chat.id, '-=-=--=-=-=-', reply_markup=markup)
 
 
 bot.polling()
