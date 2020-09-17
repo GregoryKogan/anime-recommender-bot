22e08f81b737d48112de57acd3749e66195c2d5b
G.Koganovskiy <grisha.koganovskiy@gmail.com>
2020-09-17 20:06:33 +0300

    Info button bug fixed

--- Telegram-Recommender/variables.py
+++ Telegram-Recommender/variables.py
@@ -12,7 +12,7 @@ def main_menu():
     markup = types.ReplyKeyboardMarkup(row_width=1, selective=False)
     add_anime_button = types.KeyboardButton('Rate anime')
     account_button = types.KeyboardButton('Account')
-    info_button = types.KeyboardButton('Info')
+    info_button = types.KeyboardButton('Get info')
     get_recs_button = types.KeyboardButton('Get recommendations')
     markup.add(get_recs_button,
                add_anime_button,
